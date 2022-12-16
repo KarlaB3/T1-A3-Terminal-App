7252c5c655a8b748d0a2b04ca468145a50c9b4d5
KarlaB3 <k.bucoy@gmail.com>
2022-12-16 22:29:27 +1100

    testing dictionary output

--- initial_test.py
+++ initial_test.py
@@ -1,19 +1,10 @@
 import pytest
 
-import datetime
 
-FAKE_TIME = datetime.datetime(2020, 12, 25, 17, 5, 55)
+def test_diary_display_1():
+    diary = [{"date": "01-12-22", "breakfast": "eggs", "lunch": "salad", "dinner": "soup", "snack": "fruit"}]
+    assert diary == ["01-12-22, eggs, salad, soup, fruit"]
 
-@pytest.fixture
-def patch_datetime_now(monkeypatch):
-
-    class mydatetime:
-        @classmethod
-        def now(cls):
-            return FAKE_TIME
-
-    monkeypatch.setattr(datetime, 'datetime', mydatetime)
-
-
-def test_patch_datetime(patch_datetime_now):
-    assert datetime.datetime.now() == FAKE_TIME
+def test_diary_display_2():
+    diary = [{"date": "01-12-22", "breakfast": "eggs", "lunch": "salad", "dinner": "soup", "snack": "fruit"}]
+    assert diary == ["date: 01-12-22, breakfast: eggs, lunch: salad, dinner: soup, snack: fruit"]
