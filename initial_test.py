1991576ec5ed6aa6874bad252866d6a026b08331
KarlaB3 <k.bucoy@gmail.com>
2022-12-16 23:58:34 +1100

    data validation using test csv file

--- initial_test.py
+++ initial_test.py
@@ -1,10 +1,14 @@
+# Testing expected data in .csv diary file using a test file 'test_diary.csv'
 import pytest
+import pandas as pd
+import datatest as dt
 
+@pytest.fixture(scope='module')
+@dt.working_directory(__file__)
+def df():
+    return pd.read_csv('test_diary.csv')
 
-def test_diary_display_1():
-    diary = [{"date": "01-12-22", "breakfast": "eggs", "lunch": "salad", "dinner": "soup", "snack": "fruit"}]
-    assert diary == ["01-12-22, eggs, salad, soup, fruit"]
+# Test to validate expected columns in diary .csv file
+def test_columns(df):
+    dt.validate(df.columns, {'date', 'breakfast', 'lunch', 'dinner', 'snack'})
 
-def test_diary_display_2():
-    diary = [{"date": "01-12-22", "breakfast": "eggs", "lunch": "salad", "dinner": "soup", "snack": "fruit"}]
-    assert diary == ["date: 01-12-22, breakfast: eggs, lunch: salad, dinner: soup, snack: fruit"]
---
+++ other_t3st.py
@@ -0,0 +1,7 @@
+def test_diary_display_1():
+    diary = [{"date": "01-12-22", "breakfast": "eggs", "lunch": "salad", "dinner": "soup", "snack": "fruit"}]
+    assert diary == ["01-12-22, eggs, salad, soup, fruit"]
+
+def test_diary_display_2():
+    diary = [{'date': 01-12-22, 'breakfast': eggs, 'lunch': "salad", "dinner": "soup", "snack": "fruit"}]
+    assert diary == ["date: 01-12-22", "breakfast: eggs", "lunch: salad", "dinner: soup", "snack: fruit"]
